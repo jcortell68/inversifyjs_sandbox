4d4d0e486a608998c9baba5f919e9818d8a3560f
John Cortell <j.cortell@sarc.samsung.com>
2023-07-06 19:40:29 -0500

    Proof of singleton binding

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { container } from "./inversify.config";
 import { TYPES, ISuperhero } from "./types";
+let assert = require('assert')
 
 let hero = container.get<ISuperhero>(TYPES.ISuperhero);
 hero.origin()
+
+// Proof that container dishes out a singleton for ISuperhero, as specified in
+// the bind call.
+let hero2 = container.get<ISuperhero>(TYPES.ISuperhero);
+assert(hero2 === hero)
