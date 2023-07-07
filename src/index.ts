3a5e43e2bc8c62cbd820fa9a31a8e176c92fa4a7
John Cortell <j.cortell@sarc.samsung.com>
2023-07-07 13:26:21 -0500

    Chapter cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { container } from "./inversify.config";
 import { TYPES, ISuperhero } from "./types";
-let assert = require('assert')
 
 let hero = container.get<ISuperhero>(TYPES.ISuperhero);
-hero.origin();
 hero.fight();
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,10 +1,8 @@
 import { Container } from "inversify";
 import { TYPES, ISuperhero, IWeapon } from "./types";
-import { Hammer, MarvelCharacter, Sword } from "./implementations";
+import { MarvelCharacter, Sword } from "./implementations";
 
 export let container = new Container();
 
-// Bind using transient scope. This is the default--what we get if we don't
-// specify the scope.
 container.bind<ISuperhero>(TYPES.ISuperhero).to(MarvelCharacter);
 container.bind<IWeapon>(TYPES.IWeapon).to(Sword);
