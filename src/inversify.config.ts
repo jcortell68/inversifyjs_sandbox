cdc72f89eb073e8ee717ff60fba7301a01b7ae22
John Cortell <j.cortell@sarc.samsung.com>
2023-07-07 09:48:04 -0500

    Bind to constant value

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -6,16 +6,7 @@ export let container = new Container();
 
 container.bind<ISuperhero>(TYPES.ISuperhero).to(MarvelCharacter);
 
-// Bind lethal and nonlethal weapon implementations using tags
-container.bind<IWeapon>(TYPES.IWeapon).to(Hammer).whenTargetTagged("lethal", true);
-container.bind<IWeapon>(TYPES.IWeapon).to(NerfGun).whenTargetTagged("lethal", false);
-
-// Bind a "dynamic value" that randomly produces either a lethal or non-lethal weapon.
-// This is effectively a factory as in the last example, but there's a key difference.
-// The injection point doesn't expicitly deal wth a factory. The container does, but
-// the thing being injected to has no clue that we supplied a factory.
-function weaponFactory(context: interfaces.Context) {
-    let useLethalForce = Math.random() < 0.5;
-    return context.container.getTagged<IWeapon>(TYPES.IWeapon, "lethal", useLethalForce);
-}
-container.bind<IWeapon>(TYPES.IWeapon).toDynamicValue(weaponFactory).whenTargetIsDefault();
+// Bind to a contact value. Even here we have some flexibility but the logic
+// has no container context and it has to be an expression that resulve to the
+// requested type
+container.bind<IWeapon>(TYPES.IWeapon).toConstantValue((Math.random() < 0.5) ? new Hammer() : new NerfGun());
