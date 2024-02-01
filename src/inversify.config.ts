9167bfd33ff9bd02dd3e8b56abcd8158ccaa3228
John Cortell <j.cortell@samsung.com>
2024-02-01 09:43:10 -0600

    Actually...this is how you make it work.

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,18 +4,10 @@ import { DcCharacter, MarvelCharacter } from "./implementations";
 
 export let container = new Container();
 
-container.bind<ISuperhero>(TYPES.ISuperhero).to(MarvelCharacter).inSingletonScope();
-container.bind<ISuperhero>(TYPES.ISuperhero).to(DcCharacter).inSingletonScope();
-
-// The intent here is to have the superhero singletons also serves as mortals,
-// since MarvelCharacter and DcCharacter implement IMereMortal. But
-// container.getAll<IMereMortal>(TYPES.IMereMortal) will fail with
-//    Error: Ambiguous match found for serviceIdentifier: Symbol(ISuperhero)
-//
-container.bind<IMereMortal>(TYPES.IMereMortal).toService(TYPES.ISuperhero);
-
-// Using toService in that way works only if not mult-binding.
-// I.e., if there is only one type bound to ISuperhero, then toService()
-// works as we'd hope. You can see that by commenting out one of the two lines
-// that bind to ISuperHero (lines 7-8)
+container.bind(MarvelCharacter).toSelf().inSingletonScope();
+container.bind(DcCharacter).toSelf().inSingletonScope();
+container.bind<ISuperhero>(TYPES.ISuperhero).toService(MarvelCharacter);
+container.bind<ISuperhero>(TYPES.ISuperhero).toService(DcCharacter);
+container.bind<IMereMortal>(TYPES.IMereMortal).toService(MarvelCharacter);
+container.bind<IMereMortal>(TYPES.IMereMortal).toService(DcCharacter);
 
